405ca31780a87ca93c07749939e2d25e6adc8c58
binhnhatseve <binhvu9794@gmail.com>
2016-10-12 05:59:11 -0700

    Update bndautrang.js

--- bndautrang.js
+++ bndautrang.js
@@ -117,7 +117,7 @@ jQuery(window).load(function() {
 			jQuery(divpopup).attr('id', 'myModal');
 			jQuery(divpopup).attr('class', 'linhnguyen-modal');
 			jQuery( "body" ).append(divpopup);
-			jQuery("#myModal").html("<a href='http://goo.gl/rKJWvA' target='_blank'><img src='http://faceseo.vn/bannerweb/hoc-seo-chuyen-sau.jpg' width='500px'/><br/>13 Kỹ thuật đẩy top vượt trội</a>. Học tại nhà CEO Faceseo. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			jQuery("#myModal").html("<a href='https://goo.gl/Yjl09z' target='_blank'><img src='https://2.bp.blogspot.com/-w07L_6j-5cQ/V9-Px3zw2eI/AAAAAAAAAb0/oo57gBjlEOEfGfym3Qy4T46uXLpgT0mBgCEw/s400/IMG_0589.JPG' width='500px'/><br/>máy xông hơi giá rẻ đây</a>.. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			jQuery('#myModal').linhnguyen(jQuery('#myModal').data());
 		}
 });
