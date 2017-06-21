9a29a95c685f72402af1aa7cd2b7738468ba18ab
binhnhatseve <binhvu9794@gmail.com>
2017-06-21 00:24:08 -0700

    Update new 26.js

--- new 26.js
+++ new 26.js
@@ -1,5 +1,2 @@
 <div><a href='http://mdstoryfashion.com/'><img align='right' src='https://lh3.googleusercontent.com/-aKjgZA509KM/WUnq-oJ3EBI/AAAAAAAAANE/NQ7w_aUktlY5f7PFBTCVA7hr4kXGQ3_rQCL0BGAs/w800-d-h300-p-rw/banner-web.jpg' style='margin-bottom:5px'/></a></div>       
-</div><!-- /header-inner-wrap -->
- 
-</div>
-      </div>
+
