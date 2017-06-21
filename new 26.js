c8241e6ef31317bfb29599552cb3e7573762df17
binhnhatseve <binhvu9794@gmail.com>
2017-06-21 00:35:09 -0700

    Delete new 26.js

--- new 26.js
+++
@@ -1,2 +0,0 @@
-<div><a href='http://mdstoryfashion.com/'><img align='right' src='https://lh3.googleusercontent.com/-aKjgZA509KM/WUnq-oJ3EBI/AAAAAAAAANE/NQ7w_aUktlY5f7PFBTCVA7hr4kXGQ3_rQCL0BGAs/w800-d-h300-p-rw/banner-web.jpg' style='margin-bottom:5px'/></a></div>       
-
